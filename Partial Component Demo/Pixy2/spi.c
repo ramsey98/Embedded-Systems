155b71c4497c6e75c9257d086243ced6f11c9a9e
Matthew Conway <mattwc2@vt.edu>
2020-02-26 14:07:46 -0500

    pixy callback works

--- Partial Component Demo/Pixy2/spi.c
+++ Partial Component Demo/Pixy2/spi.c
@@ -6,29 +6,30 @@
  */
 #include "spi.h"
 
-uint8_t txBufferInfo[4] = {
-                                   0xae,  // first byte of no_checksum_sync (little endian -> least-significant byte first)
-                                   0xc1,  // second byte of no_checksum_sync
-                                   0x0e,  // this is the version request type
-                                   0x00   // data_length is 2
-                                };
-
 void spiCallback(SPI_Handle handle, SPI_Transaction *transaction)
 {
    dbgOutputLoc(SPI_CALLBACK);
    if(transaction->status == SPI_TRANSFER_COMPLETED)
    {
        dbgOutputLoc(SPI_SUCCESS_TRANSFER);
-       dbgUARTStr((char *) (*transaction).rxBuf);
+       /*
+       //dbgUARTStr((char *) (*transaction).rxBuf);
+       uint8_t *rx_buffptr = *(uint8_t **)(*transaction).rxBuf;
+       dbgUARTVal(rx_buffptr[0]);
 
-       //if rx_buffer[2] == 33
+       //color connected components response
+       if(rx_buffptr[2] == 33) {
+           uint8_t packet_length = rx_buffptr[3];
+           uint8_t block_count = packet_length/CONNECTED_PACKET_LENGTH;
+           dbgUARTVal(block_count);
+       }
 
-       //if code == etc
+       //get version response
+       else if(rx_buffptr[2] == 15) {
+
+       } */
    }
-   else if(transaction->status == SPI_TRANSFER_FAILED) {
-       dbgOutputLoc(SPI_FAIL_TRANSFER);
-       halt();
-   }
+
    return;
 }
 
@@ -43,30 +44,78 @@ void spiInit()
     params.transferCallbackFxn = spiCallback;
     handle = SPI_open(CONFIG_SPI_0, &params);
 
-    int i;
-    for(i=0; i < SPI_MSG_LENGTH; i++) {
-        if(i < 4) {
-            txBuffer[i] = txBufferInfo[i];
-        } else {
-            txBuffer[i] = 0xff;
-        }
-    }
-
-    for(i=0; i < SPI_MSG_LENGTH; i++) {
-        rxBuffer[i] = 0;
-    }
-
     if (!handle) {
         halt();
     }
 }
 
-void spiTransfer()
+void setTxBuffer(uint8_t *tx_buffer, uint8_t *tx_msg, unsigned tx_length, unsigned msg_length) {
+    int i;
+    for(i=0; i < tx_length; i++) {
+        if(i < msg_length) {
+            tx_buffer[i] = tx_msg[i];
+        } else {
+            tx_buffer[i] = 0xff;
+        }
+    }
+}
+
+void setRxBuffer(uint8_t *rx_buffer, unsigned rx_length) {
+    memset((void *) rx_buffer, 0, rx_length);
+}
+
+void spiGetVersionPacket(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count) {
+    uint8_t txMsgVersion[SPI_TX_MSG_VERSION] = {
+                                                     0xae,  // first byte of no_checksum_sync (little endian -> least-significant byte first)
+                                                     0xc1,  // second byte of no_checksum_sync
+                                                     0x0e,  // this is the version request type
+                                                     0x00  // data_length is 0
+                                               };
+    setRxBuffer(rx_buffer, frame_count);
+    setTxBuffer(tx_buffer, txMsgVersion, frame_count, SPI_TX_MSG_VERSION);
+
+    spiTransfer(frame_count, rx_buffer, tx_buffer);
+}
+
+void spiSetColorPacket(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count, uint8_t r, uint8_t g, uint8_t b) {
+    uint8_t txMsgColor[SPI_TX_MSG_COLOR] = {
+                                                     0xae,  // first byte of no_checksum_sync (little endian -> least-significant byte first)
+                                                     0xc1,  // second byte of no_checksum_sync
+                                                     0x14,  // this is the version request type
+                                                     0x03,  // data_length is 0
+                                                     r,
+                                                     g,
+                                                     b
+                                               };
+    setRxBuffer(rx_buffer, frame_count);
+    setTxBuffer(tx_buffer, txMsgColor, frame_count, SPI_TX_MSG_COLOR);
+
+    spiTransfer(frame_count, rx_buffer, tx_buffer);
+}
+
+
+void spiGetConnectedBlocks(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count) {
+    uint8_t txMsgConnected[SPI_TX_MSG_CONNECTED] = {
+                                                       0xae,  // first byte of no_checksum_sync (little endian -> least-significant byte first)
+                                                       0xc1,  // second byte of no_checksum_sync
+                                                       0x20,  // this is the version request type
+                                                       0x02,   // data_length is 2
+                                                       0xff,
+                                                       0xff,
+                                                    };
+
+    setRxBuffer(rx_buffer, frame_count);
+    setTxBuffer(tx_buffer, txMsgConnected, frame_count, SPI_TX_MSG_CONNECTED);
+
+    spiTransfer(frame_count, rx_buffer, tx_buffer);
+}
+
+void spiTransfer(unsigned frame_count, uint8_t *rx_buffer, uint8_t *tx_buffer)
 {
     dbgOutputLoc(SPI_BEGIN_TRANSFER);
-    spiTransaction.count = SPI_MSG_LENGTH;
-    spiTransaction.txBuf = (void *) txBuffer;
-    spiTransaction.rxBuf = (void *) rxBuffer;
-    SPI_transfer(handle, &spiTransaction);
-    //dbgOutputLoc(SPI_WAIT_CALLBACK);
+    SPI_Transaction spi_transaction;
+    spi_transaction.count = frame_count;
+    spi_transaction.txBuf = (void *) tx_buffer;
+    spi_transaction.rxBuf = (void *) rx_buffer;
+    SPI_transfer(handle, &spi_transaction);
 }
--- Partial Component Demo/Pixy2/spi.h
+++ Partial Component Demo/Pixy2/spi.h
@@ -16,16 +16,21 @@
 #include "string.h"
 
 #define SPI_MSG_LENGTH 128
+#define SPI_TX_MSG_VERSION 4
+#define SPI_TX_MSG_CONNECTED 6
+#define SPI_TX_MSG_COLOR 7
+#define CONNECTED_PACKET_LENGTH 14
 
-void spiCallback(SPI_Handle handle, SPI_Transaction *transaction);
 void spiInit();
-void spiTransfer();
+void spiCallback(SPI_Handle handle, SPI_Transaction *transaction);
+void setTxBuffer(uint8_t *tx_buffer, uint8_t *tx_msg, unsigned tx_length, unsigned msg_length);
+void setRxBuffer(uint8_t *rx_buffer, unsigned rx_length);
+void spiGetVersionPacket(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count);
+void spiSetColorPacket(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count, uint8_t r, uint8_t g, uint8_t b);
+void spiGetConnectedBlocks(uint8_t *rx_buffer, uint8_t *tx_buffer, unsigned frame_count);
+void spiTransfer(unsigned frame_count, uint8_t *rx_buffer, uint8_t *tx_buffer);
 
 SPI_Handle      handle;
 SPI_Params      params;
-SPI_Transaction spiTransaction;
-
-uint8_t rxBuffer[SPI_MSG_LENGTH];
-uint8_t txBuffer[SPI_MSG_LENGTH];
 
 #endif /* SPI_H_ */
